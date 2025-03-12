96c0b66e444f0d1a2f9c2f71f17d514d5f5cbe20
Anna1070 <anapaula.luca@ulbsibiu.ro>
2025-03-12 19:17:31 +0200

    Project setup
    Created the project and did some of the tasks from the pdf. (Paula)

---
+++ Proiect_final/Colectare_pubela/Colectare_pubela/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Colectare_pubela.Data
+{
+    public class AppDbContext : DbContext
+    {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ Proiect_final/Colectare_pubela/Colectare_pubela/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Colectare_pubela.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
