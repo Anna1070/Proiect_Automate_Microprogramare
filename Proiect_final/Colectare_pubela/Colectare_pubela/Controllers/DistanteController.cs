8561496cfce418b1e3a62d9b7f003fe44f96aca7
Anna1070 <anapaula.luca@ulbsibiu.ro>
2025-04-10 16:33:18 +0300

    Created a new branch and a controller

---
+++ Proiect_final/Colectare_pubela/Colectare_pubela/Controllers/DistanteController.cs
@@ -0,0 +1,22 @@
+﻿using Colectare_pubela.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colectare_pubela.Controllers
+{
+    public class DistanteController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<DistanteController> _logger;
+
+        public DistanteController(AppDbContext context, ILogger<DistanteController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult CalculeazaDistante()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
